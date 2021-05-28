944b85672c4cf01d274d62b947af38810bed9b9c
gitkyla <agitsyang@gmail.com>
2021-05-28 22:30:36 +0700

    add vue config file

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      title: 'Yumrecipes',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index'],
+    },
+  },
+};
