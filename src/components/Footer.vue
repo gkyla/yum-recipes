49870a9117ad0fb065a0f7808f42d9e29ad300cd
gkyla <agitsyang@gmail.com>
2021-05-30 01:06:24 +0700

    update styling

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
     class="text-center d-flex align-items-center justify-content-center bg-secondary mt-auto"
   >
     <p class="text-white m-0 p-2">
-      Build with <i class="bi bi-heart-fill text-danger"></i> by
+      Built with <i class="bi bi-heart-fill text-danger"></i> by
       <a href="https://github.com/gkyla" class="text-white" target="_blank"
         >gkyla</a
       >
